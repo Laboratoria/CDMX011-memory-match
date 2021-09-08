f6019ec87cc59c12592b40e5b0d2e33d1133f24f
alejandraglezd <alejandra.gnzlz.dz@gmail.com>
2021-09-07 19:15:04 -0500

    view-data

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,17 @@
-//
-// Para incluir los diferentes sets de cartas podemos _importar_ el archivo
-// JavasSript que contenga el `export` correspondiente...
-//
-// import pokemon from '../data/pokemon/pokemon.js';
-// console.log(pokemon);
+
+
+// const App = () => {
+// const el = document.createElement('div');
+
+// el.className = 'App';
+// el.textContent = 'Hola mundo!';
+
+//  return el;
+// };
+
+export default App;
+
+
 //
 // O alternativamente podríamos cargar el JSON de forma asíncrona usando
 // `fetch` en el momento que consideremos necesario.
@@ -13,14 +21,3 @@
 //   .then(console.log)
 //   .catch(console.error);
 //
-
-const App = () => {
-  const el = document.createElement('div');
-
-  el.className = 'App';
-  el.textContent = 'Hola mundo!';
-
-  return el;
-};
-
-export default App;
--- src/main.js
+++ src/main.js
@@ -1,3 +1,62 @@
-import App from './components/App.js';
+// import App from './components/App.js';
+import data from '../data/webdev/webdev.js';
 
-document.getElementById('root').appendChild(App());
+const allDevs = data.items;
+// console.log(allDevs)
+
+let devsHtml = "";
+ allDevs.forEach((eachDev) => {
+     devsHtml += `<div class="container">
+     <div class="card">
+     <div class="front">
+     <img src=${eachDev.image} height="100px" width="90px"/>
+     <p>${eachDev.id}</p>
+     <div class="back">
+     <p><dev></p>
+     </div>
+     </div>
+     </div>
+     </div>`;
+ })
+ document.getElementById("root").innerHTML = devsHtml;
+
+
+
+
+
+// 1. Crear una estructura html para reutilizar en cada uno de los items
+// 2. Definir una variable para construir una función que implemente el método map,
+// 3. Crear un elemento div para poder añadir la información del objeto (createElement)
+// 4. Utilizar appendChild  (suma elementos (hijos) de html en un contenedor)
+// const root = document.getElementById('root')
+// const templateCard = (item)=> {
+// const html = `<img src=${item.image}/>`
+// const card = document.createElement("div");
+// card.innerHTML = html;
+// return card
+// }
+//forEach-template-appendchild
+
+
+ 
+
+
+
+
+
+
+
+//  const allDevs = document.getElementById("cards");
+//  devCards(data.items);
+
+//  function devCards (){
+//      allDevs.innerHTML = "";
+//      data.items.map( => {
+//      let cards = document.
+
+//      })
+//  }
+
+
+
+
