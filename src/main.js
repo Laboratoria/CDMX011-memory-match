1ae5c1c7e761aaa0d339375fe5b8834921116c4e
kati <katiacuevasj@gmail.com>
2021-09-20 10:51:14 -0500

    crea componente a titulo

--- src/main.js
+++ src/main.js
@@ -1,2 +1,7 @@
+// componente de ejemplo que muestra cómo podemos representar 
+// un componente como una función que retorna un HTMLElement
 import App from './components/App.js';
+
+document.getElementById('root').appendChild(App());
+console.log(App); 
 
