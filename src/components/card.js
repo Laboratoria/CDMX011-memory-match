adc7b5007432bdb9516c97ca160f42e47d348cf9
kati <katiacuevasj@gmail.com>
2021-10-07 16:53:12 -0500

    Hace check  for match

--- src/components/card.js
+++ src/components/card.js
@@ -1,20 +1,30 @@
 
-import {image} from './image.js'
+// import {image} from './image.js';
+
+ 
 
   
-    const card = (unPokemon, flipCard) => {
+    const card = (unPokemon, flipCard, checkForMatch,index) => {
     const divCard = document.createElement('div');
     const imagenPokemon = document.createElement('img');
     
+    
     divCard.className = 'Card';                                                    
     imagenPokemon.src = unPokemon.image;
+    divCard.dataset.id = unPokemon.id;
+    divCard.dataset.idx = index; 
     divCard.appendChild(imagenPokemon) //esté es solo un elemento por eso no necesita los parentesis
+
+    // //Aquí vamos hacer match comparando el id de las tarjetas
+    //var elId = document.querySelector("div.user-panel.main input[name='login']");
+ 
+    //Aquí volteamos el la tarjeta 
+    divCard.addEventListener('click', flipCard); 
+    divCard.addEventListener('click', checkForMatch);
+
     
-    divCard.addEventListener('click', flipCard)
-
-
-    console.log(image());
-  
+    // console.log(image());
+    
     return divCard;
   };
    
